f6a6e381749c79372e6c1c38eff4f50ad699b652
Divi-Tiru-Sai-Gopala-Pavana-Phaneendra <divi.tiru.sai.gopala.pavana.phaneendra@ibm.com>
2023-01-20 10:03:37 +0530

    regular expression for target and route fixed

--- ibm/service/metricsrouter/resource_ibm_metrics_router_route.go
+++ ibm/service/metricsrouter/resource_ibm_metrics_router_route.go
@@ -115,7 +115,7 @@ func ResourceIBMMetricsRouterRouteValidator() *validate.ResourceValidator {
 			ValidateFunctionIdentifier: validate.ValidateRegexpLen,
 			Type:                       validate.TypeString,
 			Required:                   true,
-			Regexp:                     `^[a-zA-Z0-9 -._:]+$`,
+			Regexp:                     `^[a-zA-Z0-9 \-._:]+$`,
 			MinValueLength:             1,
 			MaxValueLength:             1000,
 		},
